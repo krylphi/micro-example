3223ace1f156e5f36dd724bdaf754837b3c23286
Vsevolod Krivitskiy <vsevolod.krivitsky@itechart-group.com>
2020-06-09 11:06:05 +0300

    initial

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"example/handler"
+	"example/subscriber"
+	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
+
+	example "example/proto/example"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.service.example"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.service.example", service.Server(), new(subscriber.Example))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
